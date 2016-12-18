5d4f9927d6078338a29339beacde11633f5b8a38
Damien Asseya <dasseya1@gmail.com>
2016-12-18 11:59:45 +0000

    Add routes for Homepage and About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/home' => 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
