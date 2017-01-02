c1523f6f68fdc5ad26bf1c86c96df6de87d08cd5
Damien Asseya <dasseya1@gmail.com>
2017-01-02 12:25:31 +0000

    Add authentication methods and links to log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [edit, update, show]
+    
     def new
         @user = User.new
     end
@@ -14,12 +16,10 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
         
     end
     
     def update
-        @user = User.find(params[:id])
             if @user.update(user_params)
                 flash[:success] = "Welcome to the Alpha blog #{@user.username}"
                 redirect_to articles_path
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
         @user_articles = @user.articles.paginate(page: params[:page])
     end
         
@@ -41,5 +40,9 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id])
     end
 end
